c84f9519e48327b2d1c267022e37e5c0e49afb2c
superpatrex <jack@ding911.com>
2024-01-16 15:46:13 -0500

    Added AritificialIntelligence.cs and Utility.cs and added UI prefabs

--- 24L-Streaming-AI-To-VR-AR/Assets/Scripts/ArtificialIntelligence.cs
+++ 24L-Streaming-AI-To-VR-AR/Assets/Scripts/ArtificialIntelligence.cs
@@ -7,6 +7,10 @@ using TMPro;
 
 namespace OpenAI
 {
+    /// <summary>
+    /// The artificial intelligence class that will be used to communicate with the OpenAI API.
+    /// </summary>
+    /// /// <seealso cref="cref="MonoBehavior"/> 
     public class ArtificialIntelligence : MonoBehaviour
     {
 
@@ -16,16 +20,22 @@ namespace OpenAI
 
         private static string _userInput = "";
         private static string _latLongString = "Give me the latitude of longitude in decimals of the location to which is specified. For instance, if the user were to say \"Epcot\" or \"Take me to Epcot\"  or \"Where is Epcot\" return \"28.3765 N, 81.5494 W\". Only return the latitude and longitude.";
-        private static string _userQuestion = "";
+        private static string _funFact = "You are an artificial intelligence model that only gives fun facts about virtual reality, flight simulators, and artificial intelligence. For instance, when prompted the artificial intelligence should give a fact like \"The precursor to the flight simulator was the \"Tonneau Antoinette\"\". Only produce one have and make it succinct and making it 10-20 words. Do not respond to the user's actual questions or comments only produce facts.";
+        private static string _userQuestion = "You are the vRITA artificial intelligence that's sole function is to answer the user's questions. You will be inputted XML data and a user's question. You will return an answer to the user based on the XML data or if the the question does not directly require the XML data then just answer the question. Answers should be in the metric system.";
         private static string _openAIModel = "gpt-4-1106-preview";
 
         // Public fields
-        [SerializeField] public TMP_Text inputField;
-        [SerializeField] public TMP_Text outputField;
+        [SerializeField] public TMP_Text inputFunFactField;
+        [SerializeField] public TMP_Text outputFunFactField;
+        [SerializeField] public TMP_Text inputUserQuestionField;
+        [SerializeField] public TMP_Text outputUserQuestionField;
+        [SerializeField] public TMP_Text inputLatLongField;
+        [SerializeField] public TMP_Text outputLatLongField;
 
         // Non-public and Non-private fields
         static UnityEvent m_LatEvemt = new UnityEvent();
         static UnityEvent m_QuestionEvent = new UnityEvent();
+        static UnityEvent m_FunFactEvent = new UnityEvent();
 
 
         /// <summary>
@@ -35,6 +45,7 @@ namespace OpenAI
         {
             m_LatEvemt.AddListener(SendLatLong);
             m_QuestionEvent.AddListener(SendUserQuestion);
+            m_FunFactEvent.AddListener(SendFunFact);
         }
 
         /// <summary>
@@ -45,14 +56,28 @@ namespace OpenAI
 
         }
 
+        /// <summary>
+        /// Send the latitude and longitude to the OpenAI API. Used for the button.
+        /// </summary>
         public void SendLatLongButtonHandler()
         {
             m_LatEvemt.Invoke();
         }
 
+        /// <summary>
+        /// Send the user question to the OpenAI API. Used for the button.
+        /// </summary>
         public void SendUserQuestionButtonHandler()
         {
             m_QuestionEvent.Invoke();
+        }
+
+        /// <summary>
+        /// Send the fun fact to the OpenAI API. Used for the button.
+        /// </summary>
+        public void SendFunFactButtonHandler()
+        {
+            m_FunFactEvent.Invoke();
         }
 
         /// <summary>
@@ -60,7 +85,7 @@ namespace OpenAI
         /// <summary>
         private async void SendLatLong()
         {
-            _userInput = inputField.text;
+            _userInput = inputLatLongField.text;
 
             var newMessage = new ChatMessage()
             {
@@ -102,11 +127,11 @@ namespace OpenAI
                 if (choiceString == "null")
                 {
                     Debug.Log("Error: Not a valid question to the location");
-                    outputField.text = "Not a valid destination"; 
+                    outputLatLongField.text = "Not a valid destination"; 
                 }
                 else
                 {
-                    outputField.text = choiceString;
+                    outputLatLongField.text = choiceString;
                 }
 
             }
@@ -121,6 +146,8 @@ namespace OpenAI
         /// </summary>
         private async void SendUserQuestion()
         {
+            _userInput = inputUserQuestionField.text;
+
             var newMessage = new ChatMessage()
             {
                 Role = "system",
@@ -132,7 +159,7 @@ namespace OpenAI
             newMessage = new ChatMessage()
             {
                 Role = "user",
-                Content = _userQuestion
+                Content = _userInput
             };
 
             this._msg.Add(newMessage);
@@ -156,7 +183,57 @@ namespace OpenAI
                     Debug.Log("Error: No response from OpenAI API, choices count");
                 }
             
-                outputField.text = completionResponse.Choices[0].Message.Content.Trim();
+                outputUserQuestionField.text = completionResponse.Choices[0].Message.Content.Trim();
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log("Error: " + e);
+            }
+        }
+
+        /// <summary>
+        /// Send the fun fact to the OpenAI API.
+        /// </summary>
+        private async void SendFunFact()
+        {
+            _userInput = inputFunFactField.text;
+
+            var newMessage = new ChatMessage()
+            {
+                Role = "system",
+                Content = _funFact
+            };
+
+            this._msg.Add(newMessage);
+
+            newMessage = new ChatMessage()
+            {
+                Role = "user",
+                Content = "Fact time"
+            };
+
+            this._msg.Add(newMessage);
+
+            try
+            {
+                var completionResponse = await this._openAI.CreateChatCompletion(new CreateChatCompletionRequest()
+                {
+                    Model = _openAIModel,
+                    Messages = this._msg,
+                    MaxTokens = 100,
+                    Temperature = 1f
+                });
+
+                if (completionResponse.Choices == null)
+                {
+                    Debug.Log("Error: No response from OpenAI API, choices");
+                }
+                else if (completionResponse.Choices.Count == 0)
+                {
+                    Debug.Log("Error: No response from OpenAI API, choices count");
+                }
+            
+                outputFunFactField.text = completionResponse.Choices[0].Message.Content.Trim();
             }
             catch (System.Exception e)
             {
--- 24L-Streaming-AI-To-VR-AR/Assets/Scripts/Utility.cs
+++ 24L-Streaming-AI-To-VR-AR/Assets/Scripts/Utility.cs
@@ -1,14 +1,26 @@
+using System;
+
 namespace Utility
 {
+    /// <summary>
+    /// A class to hold utility functions.
+    /// </summary>
     public class Utility
     {
 
     }
 
-    public class LatLong
+    /// <summary>
+    /// A class to hold the latitude and longitude of a location in addition to the location name.
+    /// </summary>
+    public class LatLongLocation
     {
+        // Private fields
         private double _lat;
         private double _long;
+        private string _locationName;
+
+        // Public fields
         public double Lat
         {
             get => this._lat;
@@ -21,23 +33,57 @@ namespace Utility
             set => this._long = value;
         }
 
-        public LatLong(double lat, char latDirection, double lon, char longDirection)
+        public string LocationName
         {
-            this._lat = lat *= (latDirection == 'N') ? 1 : -1;
-            this._long = lon (latDirection == 'E') ? 1 : -1;
+            get => this._locationName;
+            set => this._locationName = value;
         }
 
-        public LatLong(string latLongString)
+        /// <summary>
+        /// Constructor for the LatLongLocation class for the individual elements of the object.
+        /// </summary>
+        /// <param name="lat"></param>
+        /// <param name="latDirection"></param>
+        /// <param name="lon"></param>
+        /// <param name="longDirection"></param>
+        /// <param name="locationName"></param>
+        public LatLongLocation(double lat, char latDirection, double lon, char longDirection, string locationName)
         {
-            
+            this.Lat = lat *= (latDirection == 'N') ? 1 : -1;
+            this.Long = lon *= (longDirection == 'E') ? 1 : -1;
+            this.LocationName = locationName;
         }
 
+        /// <summary>
+        /// Constructor for the LatLongLocation class for a string representation of the latitude and longitude and the location name.
+        /// </summary>
+        /// <param name="latLongString">String representation of the latittude and longitude and the location name such as "28.3765 N, 81.5494 W Epcot"</param>
+        public LatLongLocation(string latLongString)
+        {
+            string [] items = latLongString.Split(' ');
+
+            if (items.Length != 5)
+            {
+                throw new ArgumentException("The string representation of the latitude and longitude and the location name is not in the correct format.");
+            }
+
+            this.Lat = double.Parse(items[0]) * ((items[1][0] == 'N') ? 1 : -1);
+            this.Long = double.Parse(items[2]) * ((items[3][0] == 'E') ? 1 : -1);
+            this.LocationName = items[4];
+        }
+
+        /// <summary>
+        /// Returns a string representation of the latitude and longitude with the location name.
+        /// </summary>
+        /// <returns>A string representation of the latitude and longitude with the location name such as "28.3765 N, 81.5494 W Epcot"</returns>
         public override string ToString()
         {
             char latDirection = (this._lat >= 0) ? 'N' : 'S';
             char longDirection = (this._long >= 0) ? 'E' : 'W';
+            double lat = Math.Abs(this.Lat);
+            double lon = Math.Abs(this.Long);
 
-            return $"{this._lat} {latDirection}, {this._long} {longDirection}";
+            return $"{lat} {latDirection}, {lon} {longDirection} {this.LocationName}";
         }
 
     }
