d186951a1d7fe08da4a8ff4d2df55bde5868a94a
superpatrex <jack@ding911.com>
2024-01-28 17:50:00 -0500

    Initial. Need to add more tests.

--- 24L-Streaming-AI-To-VR-AR/Assets/Tests/MyTests.cs
+++ 24L-Streaming-AI-To-VR-AR/Assets/Tests/MyTests.cs
@@ -154,4 +154,34 @@ public class MyTests
         Assert.Throws<System.ArgumentException>(() => new LatLongLocation(lat, latDirection, lon, longDirection, locationName));
         Assert.Throws<System.ArgumentException>(() => new LatLongLocation(lat, latDirection, lon, longDirection, locationName));
     }
+
+    [UnityTest]
+    public IEnumerator OpenWeatherAPIStringPass()
+    {
+        IEnumerator enumerator = WeatherAPI.GetApiData("28.3765", "81.5494");
+
+        while (enumerator.MoveNext())
+        {
+            yield return enumerator.Current;
+        }
+
+        Assert.IsNotNull(WeatherAPI.ReturnJsonString);
+        Assert.IsTrue(!String.IsNullOrEmpty(WeatherAPI.ReturnJsonString))
+    }
+
+    [UnityTest]
+    public IEnumerator OpenWeatherAPIPass()
+    {
+        LatLongLocation item = new LatLongLocation("28.3765 N, 81.5494 W Epcot");
+        
+        IEnumerator enumerator = WeatherAPI.GetApiData(item);
+
+        while (enumerator.MoveNext())
+        {
+            yield return enumerator.Current;
+        }
+
+        Assert.IsNotNull(WeatherAPI.ReturnJsonString);
+        Assert.IsTrue(!String.IsNullOrEmpty(WeatherAPI.ReturnJsonString))
+    }
 }
