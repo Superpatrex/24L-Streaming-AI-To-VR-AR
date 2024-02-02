2d92d055a0a8259068e8cf02ea068134d6004150
superpatrex <jack@ding911.com>
2024-02-02 17:25:00 -0500

    Working on a camera switch mechanic

---
+++ 24L-Streaming-AI-To-VR-AR/Assets/Scripts/CameraViews.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraViews : MonoBehaviour
+{
+    public Camera PlaneCamera;
+    public Camera SpectorCamera;
+
+    private static CameraViews ThisInstance;
+    private Camera LastCamera;
+    private Camera CurrentCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlaneCamera.enabled = false;
+        SpectorCamera.enabled = true;
+        CurrentCamera = SpectorCamera;
+        LastCamera = PlaneCamera;
+        ThisInstance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            CurrentCamera.enabled = false;
+            CurrentCamera = SpectorCamera;
+            CurrentCamera.enabled = true;
+        }
+        else if (UnityEngine.Input.GetKeyDown(KeyCode.Alpha2))
+        {   
+            CurrentCamera.enabled = false;
+            CurrentCamera = PlaneCamera;
+            CurrentCamera.enabled = true;
+        }
+    }
+}
